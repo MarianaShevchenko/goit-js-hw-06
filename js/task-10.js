1d8e5baae9f05216ad69d593deb9bfe43996df1c
MarianaShevchenko <shevchenkomariana3@gmail.com>
2023-05-11 19:08:38 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,39 @@
+const refs = {
+  input: document.querySelector('#controls input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxes: document.querySelector('#boxes'),
+};
+
+function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(div);
+    size += 10;
+  }
+
+  refs.boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  refs.boxes.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+refs.createBtn.addEventListener('click', () => {
+  const amount = refs.input.valueAsNumber;
+  createBoxes(amount);
+});
+
+refs.destroyBtn.addEventListener('click', destroyBoxes);
